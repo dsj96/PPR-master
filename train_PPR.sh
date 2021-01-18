65db3963d501a3a8cbb44de63fb7f4aea3a8f436
dsj96 <51779172+dsj96@users.noreply.github.com>
2021-01-18 20:24:29 +0800

    Delete train_PPR.sh

--- train_PPR.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-g++ -lm -pthread -Ofast -march=native -Wall -funroll-loops -ffast-math -Wno-unused-result line.cpp -o line -lgsl -lm -lgslcblas
-g++ -lm -pthread -Ofast -march=native -Wall -funroll-loops -ffast-math -Wno-unused-result reconstruct.cpp -o reconstruct
-python3 gen_graph.py dataset/toyset/
-python3 preprocess_edges.py dataset/toyset/G_edges.txt dataset/toyset/net_edges.txt
-
-./reconstruct -train dataset/toyset/net_edges.txt -output dataset/toyset/net_dense.txt -depth 2 -threshold -400
-./line -train dataset/toyset/net_dense.txt -output dataset/toyset/vec_2nd_wo_norm.txt -binary 0 -size 16 -order 2 -negative 5 -samples 10 -threads 15
-python3 train.py dataset/toyset/
