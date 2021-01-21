a0202d0742f94ff77cbdb6e08b632720d8f4c2fa
dsj96 <51779172+dsj96@users.noreply.github.com>
2021-01-21 21:06:00 +0800

    Update train_PPR.sh

--- train_PPR.sh
+++ train_PPR.sh
@@ -4,5 +4,5 @@ python3 gen_graph.py --input_path dataset/toyset/ --epsilon 0.5 --theta 24.0
 python3 preprocess_edges.py dataset/toyset/G_edges.txt dataset/toyset/net_edges.txt
 
 ./reconstruct -train dataset/toyset/net_edges.txt -output dataset/toyset/net_dense.txt -depth 2 -threshold -400
-./line -train dataset/toyset/net_dense.txt -output dataset/toyset/vec_2nd_wo_norm.txt -binary 0 -size 16 -order 2 -negative 5 -samples 10000 -threads 15
+./line -train dataset/toyset/net_dense.txt -output dataset/toyset/vec_2nd_wo_norm.txt -binary 0 -size 16 -order 2 -negative 5 -samples 1000 -threads 15
 python3 train.py
